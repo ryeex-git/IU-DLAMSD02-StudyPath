21dbbbd7cf7891c99f86d88dcb562b7f6b196f13
dominikbayer <bayer.dominik.1405@gmail.com>
2026-03-26 18:44:37 +0100

    Initial Commit

---
+++ StudyPath/StudyPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPathApp.swift
+//  StudyPath
+//
+//  Created by Dominik on 26.03.26.
+//
+
+import SwiftUI
+
+@main
+struct StudyPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
